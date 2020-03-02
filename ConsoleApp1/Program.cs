57bc3c99f496b9fd8654984f8530303b7c5b95d1
VitaliyBondarenkoYr <elementpein@gmail.com>
2020-03-02 10:37:51 +0200

    ssss

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int a = 4, b = 5;
             Console.WriteLine("Hello");
+
+            a = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"A is ={a}, b = {b}");
             Console.ReadKey();
         }
     }
